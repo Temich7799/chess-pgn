bcad4054c11c38d8ebad12424f91476030d591e3
Temich7799 <artemgl9999@gmail.com>
2024-04-06 02:23:21 +0300

    getMapsFromSteps

--- src/utils/getMapsFromSteps.ts
+++ src/utils/getMapsFromSteps.ts
@@ -1,29 +1,37 @@
 import { FieldSquare } from "../ts/FieldSquareType";
 import { PositionsMap } from "../ts/PositionsMapType";
 import { Step } from "../ts/StepType";
+import getAllPositions from "./getAllPositions";
+import splitArrayToChunks from "./splitArrayToChunks";
 
 export default function getMapsFromSteps(initialMap: PositionsMap, steps: Array<Step>): Array<PositionsMap> {
 
-    // return steps.map(([movedFigure, figureNewPosition, makesHit]: Step, stepIndex: number) => {
+    const maps: Array<PositionsMap> = [];
+    const squarePositions = getAllPositions();
 
-    //     const isWhiteTurn = stepIndex % 2;
+    steps.forEach(([movedFigure, figureNewPosition, makesHit]: Step, stepIndex: number) => {
 
-    //     return initialMap.map((row: Array<FieldSquare>) => row.map((figurePosition: FieldSquare) => {
+        const positionsMap = JSON.parse(JSON.stringify((maps[maps.length - 1] || initialMap).flat()));
 
-    //         const { figure, type, position: figureOldPosition } = figurePosition;
+        const isWhiteTurn = !(stepIndex % 2);
+        const newPositionIndex = squarePositions.indexOf(figureNewPosition) + 1;
 
-    //         if ((isWhiteTurn && type === 'white') || (isWhiteTurn && type === 'black')) {
+        positionsMap.forEach((fieldSquare: FieldSquare, squareIndex: number) => {
 
-    //             return {
-    //                 figure: !figure ? movedFigure : (figure === movedFigure) && null,
-    //                 type,
-    //                 position: figureNewPosition
-    //             }
-    //         }
-    //         else return { figure, type, position: figureOldPosition }
-    //     })
-    //     ) as PositionsMap
-    // });
+            const oldPositionIndex = squarePositions.indexOf(fieldSquare.position) + 1;
 
-    return []
+            if (newPositionIndex === squareIndex + 1) {
+                if (positionsMap[squareIndex].figure) {
+                    console.log(positionsMap[squareIndex])
+                }
+                else {
+                    positionsMap[squareIndex].figure = { type: movedFigure, color: isWhiteTurn ? 'white' : 'black' }
+                }
+            }
+        });
+
+        maps.push(splitArrayToChunks(positionsMap, 8) as PositionsMap);
+    });
+
+    return maps;
 }
